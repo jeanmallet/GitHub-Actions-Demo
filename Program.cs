bc9c097d44e11102cbb3c04abc366fe46b4c7c2b
Jean Mallet <jean.eugene@stone.com.br>
2023-01-27 15:44:30 -0300

    Created a minimum API sample

---
+++ Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/greeting", () => "Hello from .NET");
+app.MapGet("/time/utc", () => Results.Ok(DateTime.UtcNow));
+
+app.Run();
