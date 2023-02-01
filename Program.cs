3bd8a656200bf4e12a233faa6f77abe120567464
Yan Nascimento <yan.nascimento@stone.com.br>
2023-02-01 10:24:59 -0300

    Changing file for test failure

--- Program.cs
+++ Program.cs
@@ -5,6 +5,6 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.MapGet("/greeting", () => "Hello from .NET");
-app.MapGet("/time/utc", () => Results.Ok(DateTime.UtcNow));
+            app.MapGet("/time/utc", () => Results.Ok(DateTime.UtcNow));
 
 app.Run();
